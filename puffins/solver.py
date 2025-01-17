f7e2c9e324cb8fede81bcf1c5075013caf605ec0
Cole Johnston <johnston.ccj@gmail.com>
2025-01-17 19:06:50 +0000

    update to solvers formatting all functions now use np.linalg.lstsq

--- puffins/solver.py
+++ puffins/solver.py
@@ -4,76 +4,179 @@ np.random.seed(18443280306) # The General Insurance phone number to enforce repr
 RCOND = 1e-14 # condition number below which `np.linalg.lstsq()` zeros out eigen/singular values
 
 
+def solve_wls(X, y, W=None):
 
-def solve_ridgeRegression(X, y, alpha, weights=None):
+    '''
+    Weighted least squares solver.
+    -------------------------------
 
-    if weights is None:
-        weights = np.ones(X.shape[0])
+    Parameters:
+    X: Design matrix
 
-    XTCinv = X.T / weights
-    XTCinvX = XTCinv @ X
-    XTCinvY = XTCinv @ y
-        
-    idd = np.diag_indices_from(XTCinvX)
-    XTCinvX[idd] += alpha
+    y: Target values
+
+    W: Weights for the design matrix
+       Assumes matrix with diagonal elements as 1/inverse variance on Y
+
+    Returns:
+    beta_hat: Solved coefficients (betas)
+
+    NOTES:
+    - If W is not provided, it assumes OLS
+    - If W is provided, it assumes GLS
+    - If n > p, it assumes overdetermined and uses the normal equation
+    - If n < p, it assumes underdetermined and uses the normal equation
+
+    TODO: 
+    - Implement the case where W is derived from a non-diagonal covariance matrix
     
-    betas = np.linalg.solve(XTCinvX, XTCinvY)
-    return betas
-
-
-def solve_fwols(X, omegas, y, width=None, weighted=False, weights=None):
+    '''
 
     n, p = X.shape
 
-    if width is None:
-        width = 0.1 / (2.*np.pi)
-    else:
-        width /= (2. * np.pi)
-
+    if W is None:
+        W = np.ones(n)
     
+    if n > p: # Overdetermined
+        # beta_hat = (X.T * W * X)^-1 * X.T * W * y
+        XTW = X.T * W
+        return np.linalg.lstsq(XTW @ X, XTW @ y, rcond=RCOND)[0]
+
+    else: # underdetermined
+        # beta_hat = W * X.T (X @ W @ X.T)^-1 y
+        return W * X.T @ np.linalg.lstsq( X * W @ X.T , y, rcond=RCOND)[0]
+
+
+
+def solve_generalRidge(X, y, alpha, W=None):
+    '''
+    Generalized least squares solver.
+    -------------------------------
+
+    Parameters:
+    X: Design matrix
+
+    y: Target values
+
+    W: Weights for the design matrix
+       Assumes matrix with diagonal elements as 1/inverse variance on Y
+
+    Returns:
+    beta_hat: Solved coefficients (betas)
+
+    NOTES:
+    - If W is not provided, it assumes OLS
+    - If W is provided, it assumes GLS
+    - If n > p, it assumes overdetermined and uses the normal equation
+    - If n < p, it assumes underdetermined and uses the normal equation
+
+    TODO: 
+    - Implement the case where W is derived from a non-diagonal covariance matrix
+
+    '''
+
+    n, p = X.shape
+
+    if W is None:
+        W = np.ones(n)
+    
+    XTW = X.T * W
+    XTWX = XTW @ X
+
+    XTWX[np.diag_indices_from(XTWX)] += alpha
+    return np.linalg.lstsq(XTWX, XTW @ y, rcond=RCOND)[0]
+
+
+def solve_fwols(X, y, FW, W=None):
+
+    '''
+    Feature weighted least squares solver.
+    -------------------------------
+    Parameters:
+    X: Design matrix
+        np.ndarray of shape (n, p)
+    
+    y: Target values
+        np.ndarray of shape (n,)
+    
+    L: Feature weights
+        np.ndarray of shape (p,)
+    
+    W: Weights for the targets
+        np.ndarray of shape (n,)
+        Assumes matrix with diagonal elements as 1/inverse variance on Y
+    
+    Returns:
+    beta_hat: Solved coefficients (betas)
+        np.ndarray of shape (p,)
+
+    NOTES:
+    - If weights is not provided, it assumes FWOLS
+    - W = 1/C^2, where C is the uncertainty on the target values
+
+    TODO: 
+    - Implement the case where W is derived from a non-diagonal covariance matrix
+
+    '''
+
+    n, p = X.shape
+
+    if W is None:
+        weights = np.ones(n)
+    if L is None:
+        L = np.ones(p)
+
+
     ## Assumes the approximation of a periodic version of the Matern 3/2 kernal
     # Implement eq. 23 from https://arxiv.org/pdf/2101.07256
     # In this case, we're going to use the same X for our predictions
     # however, we could propose a new X* where we want to predict
     # the regression at times t*
-    if weighted:
-        Cinv = np.linalg.inv(np.diag(weights))
-        A = X.T@Cinv@X
-        B = X.T@Cinv@y
-    else:
-        A = X.T@X
-        B = X.T@y
-    
-    idd = np.diag_indices_from(A)
-    A[idd] += ( (width**2) * (omegas**2) + 1 )
-    betas = np.linalg.solve( A, B)
-    return betas
 
+    if n > p: # Overdetermined
+        # beta_hat = (X.T * W * X + L)^-1 * X.T * W * y 
+        # Identical to the generalized ridge regression! 
+        # We just use special weights to approximate a Gaussian Process regression
 
-def solve_wls(xs, ys, ws=None):
-    n, p = xs.shape
-    if ws is None:
-        ws = np.ones(n)
-    return np.linalg.lstsq(ws[:, None] * xs , ws * ys, rcond=RCOND)[0].T
+        XTW = X.T * weights
+        XTWX= XTW @ X
+        XTWX[np.diag_indices_from(XTWX)] += L
+        return np.linalg.lstsq(XTWX, XTW @ y, rcond=RCOND)[0]
 
-def solve_ols(X, y, weights=None):
+    else: # underdetermined
+        # beta_hat = L^-1 @ X.T (X @ L^-1 @ X.T + 1/W)^-1 y
 
-    if weights is not None:
-        Cinv = np.linalg.inv(np.diag(weights))
-        betas = np.linalg.solve(X.T@Cinv@X, X.T@Cinv@y)
-    else:
-        betas = np.linalg.solve(X.T @ X, X.T @ y)
-
-    return betas
+        LinvXT = (X / Lambdadiag).T
+        XLinvXT = X @ LinvXT
+        XLinvXT[np.diag_indices(n)] += 1./W
+        return LinvXT @ np.linalg.lstsq(XLinvXT, y, rcond=RCOND)[0]
 
 
 
 
-def train_feature_weighted_ols(xs, ys, ws=None):
-    n, p = xs.shape
-    if ws is None:
-        ws = np.ones(p)
-    return np.linalg.lstsq(xs * ws[None, :], ys, rcond=RCOND)[0].T * ws
+def solve(X, y, method="wls", **kwargs):
+    """
+    General solver wrapper.
+
+    Parameters:
+    - X (np.ndarray): Design matrix.
+    - y (np.ndarray): Target values.
+    - method (str): Solver method. Options are 'ols', 'ridge', 'fwols'.
+    - **kwargs: Additional arguments for the specific solver.
+
+    Returns:
+    - np.ndarray: Solved coefficients (betas).
+    """
+    solvers = {
+        "wls": solve_wls,
+        "ridge": solve_generalRidge,
+        "fwols": solve_fwols,
+    }
+
+    if method not in solvers:
+        raise ValueError(f"Unknown method '{method}'. Supported methods: {list(solvers.keys())}")
+
+    return solvers[method](X, y, **kwargs)
 
 
 
