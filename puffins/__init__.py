af9faf17a29674544021fc468cee82de215b3cc8
Cole Johnston <johnston.ccj@gmail.com>
2025-01-17 19:27:01 +0000

    updated __init__ to avoid circular imports

--- puffins/__init__.py
+++ puffins/__init__.py
@@ -1,5 +1,6 @@
-# __all__ = ['solver', 'utils', 'periodogram']
+# __all__ = ['solver', 'utils', 'weight_functions', 'timeseries']
 
 from . import solver
 from . import utils
-from . import periodogram
+from . import weight_functions
+from . import timeseries
