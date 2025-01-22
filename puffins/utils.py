b2f228bfe60340575c0d164aad8d9185ffe8d36a
Cole Johnston <johnston.ccj@gmail.com>
2025-01-22 15:00:56 +0000

    major update and renaming construct_design_matrix function

--- puffins/utils.py
+++ puffins/utils.py
@@ -20,13 +20,12 @@ def construct_design_matrix(x, basis_functions=None, feature_embedding=None, **k
 
     Returns
     -------
-    X : np.ndarray
+    design_matrix : np.ndarray
         The design matrix
-    additional_outputs : dict
+    feature_weights : dict
         Additional outputs specific to the feature embedding, if applicable
     """
-    design_matrix_components = []
-    feature_weights = []
+
     if basis_functions:
         n_bases = len(basis_functions)
     else:
@@ -46,7 +45,10 @@ def construct_design_matrix(x, basis_functions=None, feature_embedding=None, **k
     # Include basis functions if provided
     if basis_functions is not None:
         for i,basis in enumerate(basis_functions):
-            design_matrix[i:,].append(basis(x))
+            if basis == basis_polynomial:
+                design_matrix[i:,] = basis(x, kwargs.get("degree", 1))
+            else:
+                design_matrix[i:,] = basis(x)
 
     # Include feature embedding if specified
     if feature_embedding:
@@ -60,66 +62,10 @@ def construct_design_matrix(x, basis_functions=None, feature_embedding=None, **k
             design_matrix[:,n_bases+1::2] = basis_sine(x,omegas)
             feature_weights[n_bases+1::2] = omegas
             
-    if not design_matrix_components:
+    if not basis_functions and not feature_embedding:
         raise ValueError("Either basis_functions or feature_embedding must be provided.")
 
-    # Combine all components into the design matrix
-    # X = np.column_stack(design_matrix_components)
     return design_matrix,  feature_weights
-
-
-
-def construct_design_matrix_legacy(x, period, n_harmonics):
-
-    """
-    Parameters
-    ----------
-    x : np.ndarray
-        The times of the observations
-    period : float
-        The period of the signal that we're trying to model
-    n_harmonics : int
-        The number of harmonics to include in the design matrix
-    
-    Returns
-    -------
-    X : np.ndarray
-        The design matrix
-    omega_list : np.ndarray
-        The list of frequencies that the design matrix is sensitive to
-    """
-
-    # We're going to do this as a fourier series, so our design matrix
-    # necessarily needs to be the (1 cos() sin()) pairs for each
-    # harmonic of the orbital period that we're considering
-
-    # First, we're going to get all the possible cyclic frequencies
-    # that we're concerned with, i.e. the N harmonics of the orbital
-    # frequency
-    omegas = 2. * np.pi * np.arange(1, n_harmonics + 1) / period
-
-    # Next, we instantiate the design matrix to have 2 * n_harmonics + 1 columns
-    # since we want an offset term and sin+cos term for each harmonic
-    X = np.ones((len(x), 2 * n_harmonics + 1))
-
-    # We do the same to keep track of the actual frequencies in case we
-    # want to do the approximate GP regression
-    omega_list = np.zeros(2 * n_harmonics + 1)
- 
-    # Consider the 0-frequency base term
-    #X[:,0] = 1.
-    omega_list[0] = 0.
-
-    # Populate the cosine terms
-    X[:,1::2] = np.cos(omegas[None, :] * x[:, None]) # I'm dyin heah ## original hogg quote, will not be removing.
-    omega_list[1::2] = omegas
-
-    # Populate the cosine terms
-    X[:,2::2] = np.sin(omegas[None, :] * x[:, None])
-    omega_list[2::2] = omegas
-
-    return X, omega_list
-
 
 
 def sort_on_x(x,y,yerr):
