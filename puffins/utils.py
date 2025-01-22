ac5ae668b060d5ef75c7635df0f08641fc3f0d14
Cole Johnston <johnston.ccj@gmail.com>
2025-01-22 13:09:58 +0000

    included new construct_design_matrix function with basis functions and embeddings

--- puffins/utils.py
+++ puffins/utils.py
@@ -1,7 +1,75 @@
 import numpy as np
 
+from .basis_functions import *
 
-def get_design_matrix(x, period, n_harmonics):
+
+def construct_design_matrix(x, basis_functions=None, feature_embedding=None, **kwargs):
+    """
+    Generate a design matrix based on provided basis functions and/or feature embeddings.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        The times of the observations
+    basis_functions : list of callables, optional
+        List of functions to transform time points into features
+    feature_embedding : str, optional
+        Type of feature embedding to use (e.g., "fourier")
+    kwargs : dict
+        Additional parameters for the feature embedding (e.g., period, n_harmonics)
+
+    Returns
+    -------
+    X : np.ndarray
+        The design matrix
+    additional_outputs : dict
+        Additional outputs specific to the feature embedding, if applicable
+    """
+    design_matrix_components = []
+    feature_weights = []
+    if basis_functions:
+        n_bases = len(basis_functions)
+    else:
+        n_bases = 0
+    if feature_embedding:
+        if feature_embedding == 'fourier':
+            n_embeddings = 2 * kwargs.get('n_harmonics', 1)
+        elif feature_embedding == 'spherical_harmonics':
+            n_embeddings = kwargs.get('n_harmonics', 1)
+    else:
+        n_embeddings = 0
+  
+    n_features = n_bases + n_embeddings
+    design_matrix = np.ones((len(x), n_features))
+    feature_weights = np.zeros(n_features)
+  
+    # Include basis functions if provided
+    if basis_functions is not None:
+        for i,basis in enumerate(basis_functions):
+            design_matrix[i:,].append(basis(x))
+
+    # Include feature embedding if specified
+    if feature_embedding:
+        if feature_embedding == "fourier":
+            period = kwargs.get("period", 1.0)
+            n_harmonics = kwargs.get("n_harmonics", 1)
+            omegas = 2. * np.pi * np.arange(1, n_harmonics + 1) / period
+
+            design_matrix[:,n_bases::2] = basis_cosine(x,omegas)
+            feature_weights[n_bases::2] = omegas
+            design_matrix[:,n_bases+1::2] = basis_sine(x,omegas)
+            feature_weights[n_bases+1::2] = omegas
+            
+    if not design_matrix_components:
+        raise ValueError("Either basis_functions or feature_embedding must be provided.")
+
+    # Combine all components into the design matrix
+    # X = np.column_stack(design_matrix_components)
+    return design_matrix,  feature_weights
+
+
+
+def construct_design_matrix_legacy(x, period, n_harmonics):
 
     """
     Parameters
@@ -53,11 +121,12 @@ def get_design_matrix(x, period, n_harmonics):
     return X, omega_list
 
 
+
 def sort_on_x(x,y,yerr):
-        zipp = list(zip(x,y,yerr))
-        zipp.sort(key=lambda x:x[0])
-        x,y, yerr = list(zip(*zipp))
-        return x, y, yerr
+    zipp = list(zip(x,y,yerr))
+    zipp.sort(key=lambda x:x[0])
+    x,y, yerr = list(zip(*zipp))
+    return x, y, yerr
 
 
 def cross_validate_hyperparams(x, y, yerr, period, K, n_folds=5, n_trials=50):
