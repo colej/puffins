d81d8d166043cb0a8446c918d3881cbc6ef3d5aa
Cole Johnston <johnston.ccj@gmail.com>
2025-01-22 13:09:27 +0000

    updated sine and cosine to take arrays and be broadcastable

---
+++ puffins/basis_functions.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def basis_constant(x):
+    return np.ones_like(x)
+
+
+def basis_linear(x):
+    return x
+
+
+def basis_quadratic(x):
+    return x**2
+
+
+def basis_polynomial(x, degree):
+    return np.power(x,degree)
+
+
+def basis_cosine(x, omega):
+    return np.cos(omega[None,:] * x[:,None])
+
+
+def basis_sin(x, omega):
+    return np.sin(omega[None,:] * x[:,None])
+
+
+# TODO:
+# - Implement spherical harmonic basis functions
